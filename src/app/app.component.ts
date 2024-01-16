b0239b68aecbdc96757a777431ec4613b12ee04e
Harshal-creator <harshalshelar8251@gmail.com>
2024-01-16 17:02:20 +0530

    additional data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -21,9 +21,9 @@ export class AppComponent implements OnInit {
 
     this.languageService.initLanguage()
 
-    this.titleService.setTitle("Muffeez Khalid | MEAN Stack Developer | MERN Stack Developer | Software Engineer");
+    this.titleService.setTitle("Harshal Shelar | MEAN Stack Developer | Angular Developer | Software Engineer");
     this.metaService.addTags([
-      { name: 'keywords', content: 'Frontend, MEAN Stack Developer , MERN Stack Development , Software Engineer, software, developer' },
+      { name: 'keywords', content: 'Frontend, MEAN Stack Developer , Angular Development , Software Engineer, software, developer' },
       { name: 'description', content: 'As a software engineer with expertise in both MEAN and MERN stacks, I have a comprehensive understanding of full-stack web development. My strong foundation in JavaScript allows me to effectively work with front-end technologies such as Angular and React, as well as back-end technologies such as Node.js and Express.' },
     ]);
     
